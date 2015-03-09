6ce9886247aa7eb6609db385188add46eb60bc20
Ahmad Hesham <ahmadhesham285@gmail.com>
2015-03-09 16:16:26 +0200

    Tutuorial Files added

---
+++ javaTest.js
@@ -0,0 +1,7 @@
+var mylinks=document.getElementsByTagName('a');
+console.log("Links: ", mylinks.length);
+
+var mainheader= document.getElementById("main-header");
+mainheader.setAttribute("align","center");
+console.log(mainheader.innerHTML);
+
