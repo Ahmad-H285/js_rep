a3dde99a95403387b89e48fe7da932b74da35835
Ahmad Hesham <ahmadhesham285@gmail.com>
2015-03-10 16:43:30 +0200

    Events finished

--- javaTest.js
+++ javaTest.js
@@ -7,12 +7,29 @@ console.log(mainheader.innerHTML);
 
 /*document.onclick= function(){
 	alert("You clicked somewhere in the document");
-}
+};
 */
 
 myImage= document.getElementById("main-image");
 
 myImage.onclick= function(){
 	alert("You clicked somewhere in the image");
+};
+
+var emailField= document.getElementById("email");
+
+emailField.onfocus= function(){
+	if(emailField.value=="Your Email")
+	emailField.value= "";	
 }
 
+emailField.onblur= function(){
+	if(emailField.value=="")
+	emailField.value= "Your Email";		
+}
+
+function simpleMessage(){
+	alert("Time out");
+}
+
+setTimeout(simpleMessage,5000);
